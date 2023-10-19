dfce26fa467288a2e1d3569a6f1efa54310b7bb0
Arnaud Grignard <agrignard@gmail.com>
2023-10-19 13:44:05 +0200

    add a start script

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/www'));
+const port = process.env.PORT || 3000;
 
-app.listen('3000');
-console.log('working on 3000');
+app.listen(port);
+console.log('Starting serve on working at' + port);
