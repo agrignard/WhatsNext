efc64f43061f3a639f77cc06d4c99142801694b1
Arnaud Grignard <agrignard@gmail.com>
2023-10-24 15:43:09 +0200

    Parsing from Excel OK, Chart for nb event per place and nb event per day, dev mode and dark mode

---
+++ datacleaning.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+
+
+//Getting the coordinate of each place 
+const placeToCoord = new Map();
+const placeGeoJSONPath = 'www/lyon_place.geojson';
+fs.readFile(placeGeoJSONPath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading existing GeoJSON:', err);
+        return;
+    }
+    const placeGeoJSONPath = JSON.parse(data);
+    for (var i = 0; i < placeGeoJSONPath.features.length; i++) {
+        placeToCoord.set(placeGeoJSONPath.features[i].properties.title,placeGeoJSONPath.features[i].geometry.coordinates);
+    }
+});
+
+// Getting the original source file for event
+const existingGeoJSONPath = 'www/lyon_mapzik.geojson';
+// Read the existing GeoJSON file
+fs.readFile(existingGeoJSONPath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading existing GeoJSON:', err);
+    return;
+  }
+
+  // Parse the existing GeoJSON data
+  const existingGeoJSON = JSON.parse(data);
+
+  const csvFilePath = 'www/new_event.csv';
+  const csvData = fs.readFileSync(csvFilePath, 'utf8');
+  const table = csvData.split('\n').slice(1);
+
+  table.forEach(row => {
+    const columns = row.split(','); 
+    const place=columns[0];
+    const title=columns[1];
+    const time=parseInt(columns[2]);
+    const size=parseInt(columns[3]);
+    const style=columns[4];
+    const url=columns[5];
+    console.log(place,title,time,size,style,url);
+
+   const newFeature = {
+    type: 'Feature',
+    id:parseInt(time*Math.random()),
+    geometry:{
+        type: 'Point',
+        coordinates: placeToCoord.get(place),
+    },
+    properties:{
+        "title": title,
+        "size": size,
+        "icon": "marker-15",
+        "place": place,
+        "style": style,
+        "time":time,
+        "description":"<a href='"+url+"' target='blank'title='Opens in a new window'>Prog</a>"
+    },
+    };
+    existingGeoJSON.features.push(newFeature);
+})
+
+
+
+  // Define the path to the modified GeoJSON file
+  const modifiedGeoJSONPath = 'www/lyon_mapzik.geojson';
+  // Save the modified GeoJSON to a new file
+  fs.writeFile(modifiedGeoJSONPath, JSON.stringify(existingGeoJSON), 'utf8', (err) => {
+    if (err) {
+      console.error('Error saving modified GeoJSON:', err);
+    } else {
+      console.log('Modified GeoJSON saved to', modifiedGeoJSONPath);
+    }
+  });
+});
