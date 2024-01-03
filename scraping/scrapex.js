3a4c29875a61ee0b460d7ace72f53ad11b914719
Tri Nguyen-Huu <tri.nguyen-huu@ird.fr>
2024-01-03 16:20:34 +0100

    update scrapex

--- scraping/scrapex.js
+++ scraping/scrapex.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
+const fs2 = require('fs').promises;
 const { parse, isValid } = require('date-fns');
+const cheerio = require('cheerio');
 
 // Chemin vers le fichier à lire
 const filePath = './venues-test.json';
@@ -10,191 +12,161 @@ var out="";// = "PLACE,TITRE,UNIX,SIZE,GENRE,URL";
 var outFile = "generated/scrapexResult.csv";
 var months;
 
-
-fs.readFile(dateConversionFile, 'utf8', (erreur, fileContent) => {
-  if (erreur) {
-      console.error("Erreur lors de la lecture du fichier :", erreur);
-      return;
-  }
-
-  try {
-      months = JSON.parse(fileContent);  
+fs2.readFile(dateConversionFile, 'utf8')
+  .then((fileContent) =>{
+    try {
+      months = JSON.parse(fileContent); 
     } catch (erreur) {
       console.error("Erreur de parsing JSON pour les conversions de dates :", erreur.message);
     }
- 
-
-});
-
-var fileContent;
-console.log("\n\n\n\n\n");
-
-
-
-// Lecture du fichier de manière asynchrone
-fs.readFile(filePath, 'utf8', (erreur, fileContent) => {
-    if (erreur) {
-        console.error("Erreur lors de la lecture du fichier :", erreur);
-        return;
-    }
-
-    try {
+    fs2.readFile(filePath, 'utf8')
+    .then((fileContent) =>{
+      try {
         // Parser le texte JSON
         const venues = JSON.parse(fileContent);
-      
         scrapFiles(venues);
-
-        
       } catch (erreur) {
-        console.error("Erreur de parsing JSON :", erreur.message);
+        console.error("Erreur de parsing des lieux :", erreur.message);
       }
-   
-
-})
-
-setTimeout(function() {
-  console.log('Scrap fini avec succex !!');
-  fs.writeFileSync(outFile, out, 'utf-8', { flag: 'w' });
-}, 2000);
-
-
-
-
+    })
+    console.log("\n\n\n\n\n");
+  })
+  .catch((erreur ) => {
+    console.error("Erreur lors de la lecture des fichiers de configuration :", erreur);
+  });
 
 async function scrapFiles(venues) {
-  
-
+//  await Promise.all(venues.map(analyseFile));
   for (const venue of venues) {
     await analyseFile(venue);
   }
-
-  //console.log("Fin de la boucle.");
+  console.log('Scrap fini avec succex !!\n\n');
 }
+
 
 
 async function analyseFile(venue) {
-  // Simuler une opération asynchrone
-  return new Promise((resolve) => {
-    setTimeout(() => {
-        var events,eventDate,eventName,unixDate,eventURL;
-
-        // Afficher le numéro de l'objet
-        console.log();
-        console.log('\x1b[32m%s\x1b[0m', `******* Venue: ${venue.name}  *******`);
-        const inputFile = sourcePath+venue.name+".html";
-        fs.readFile(inputFile, 'utf8', (erreur, venueContent) => {
-          if (erreur) {
-              console.error("Erreur lors de la lecture du fichier : "+venue.name, erreur);
-              return;
-          }
-          //console.log(venueContent);
-          try{
-            try{
-              const regexDelimiter = new RegExp(venue.eventsDelimiter, 'g');
-              events = venueContent.match(regexDelimiter);
-              console.log("total number of events: " + events.length);
-            }catch(err){
-              console.log('\x1b[31m%s\x1b[0m', 'Délimiteur mal défini pour '+venue.name);
-            }
-
-            const regexDate = new RegExp(venue.eventDate);
-            const regexName = new RegExp(venue.eventName);
-            const regexURL = new RegExp(venue.eventURL);
-            const dateFormat = venue.dateFormat;
-
-            
-            if (!('eventDate' in venue) || !('eventName' in venue) || !('dateFormat' in venue)){
-              console.log('\x1b[33m%s\x1b[0m', 'Pas de regexp défini pour '+venue.name);
-
-            }else{
-              for (eve of events){
-                try{
-                  const res = eve.match(regexDate);
-                  eventDate = "";
-                  for (let i = 1; i <= res.length-1; i++) {
-                    if (i > 1){
-                      eventDate += '-';
-                    }
-                    eventDate += res[i];
-                  }
-                  
-                  console.log(eventDate);
-                } catch(err){
-                  console.log('\x1b[31m%s\x1b[0m', 'Erreur regexp sur la date pour '+venue.name);
-                }
-                try{
-                  const res = eve.match(regexName);
-                  eventName = "";
-                  for (let i = 1; i <= res.length-1; i++) {
-                    if (!(res[i] === undefined)){
-                      if (i > 1){
-                        eventName += ' ';
-                      }
-                      eventName += res[i];
-                    }
-                  }
-                  eventName = removeBlanks(eventName);
-//                  eventName = eve.match(regexName)[1];
-                }catch(err){
-                  console.log('\x1b[31m%s\x1b[0m', 'Erreur regexp sur le nom de l\'événement pour '+venue.name);
-                }
-                eventDate = convertDate(eventDate);
-               // console.log(eventDate);
-                const oldDate = eventDate;
-                eventDate = parse(eventDate, dateFormat, new Date());
-                  if (!isValid(eventDate)){
-                    console.log('\x1b[31m%s\x1b[0m', 'Format de date invalide pour '+venue.name+': '+oldDate);
-                  }else{
-                    unixDate = eventDate.getTime();
-                 //   console.log(unixDate);
-                  }
-                console.log(eventName);
-                if ('eventURL' in venue){
-                  try{
-                    eventURL = eve.match(regexURL)[1];
-                    if (!(eventURL === undefined)){
-                      eventURL = venue.baseURL+eventURL;
-                    }
-                    console.log(eventURL);
-                  }catch(err){
-                    console.log('\x1b[33m%s\x1b[0m', 'URL non fonctionnelle pour '+venue.name);
-                  }
-               //   out = out+venue.name,','+eventName;
-                  out = out+venue.name+','+eventName+','+unixDate+',100,'+eventURL+'\n';
-                }
-                console.log();
-              }  
-            }
-            
-            
-          //  const regexEventName = new RegExp(venue.eventName);
-          //  const eventName = events.match(regex)[1];
-          //  console.log(eventName);
-          }catch(error){
-            console.log("Pas de regex pour "+venue.name,error);
-          }
-          console.log("\n\n");
-          
-        });
+  var events,eventDate,eventName,unixDate,eventURL, venueContent;
+  const inputFile = sourcePath+venue.name+".html";
+  try{
+    venueContent = await fs2.readFile(inputFile, 'utf8');
+  }catch (erreur){
+    console.error("Erreur lors de la lecture du fichier local de :",venue.name, erreur);
+  }
+  console.log();
+  console.log('\x1b[32m%s\x1b[0m', `******* Venue: ${venue.name}  *******`);
+  try{
+    try{
+      if (venue.hasOwnProperty('eventsDelimiter')){
+        const regexDelimiter = new RegExp(venue.eventsDelimiter, 'g');
+        events = venueContent.match(regexDelimiter);
+      }else{
+        const eventAnchor = venue.eventsAnchor;
+        const $ = cheerio.load(venueContent);
   
-      resolve();
-    }, 1000); // Une seconde de délai
-  });
+        events = [];
+        $('.'+venue.eventsAnchor).each((index, element) => {
+          let ev = $(element).html();
+          events.push(ev);
+        });
+      }
+
+      console.log("total number of events: " + events.length);       
+    }catch(err){        
+      console.log('\x1b[31m%s\x1b[0m', 'Délimiteur mal défini pour '+venue.name);      
+    }
+
+    const regexDate = new RegExp(venue.eventDate);
+    const regexName = new RegExp(venue.eventName);
+    const regexURL = new RegExp(venue.eventURL);
+    const dateFormat = venue.dateFormat;
+    
+    if (!('eventDate' in venue) || !('eventName' in venue) || !('dateFormat' in venue)){
+      console.log('\x1b[33m%s\x1b[0m', 'Pas de regexp défini pour '+venue.name);      
+    }else{
+      for (eve of events){
+        try{
+          const res = eve.match(regexDate);
+          eventDate = "";
+          for (let i = 1; i <= res.length-1; i++) {
+            if (!(res[i] === undefined)){
+              if (!(eventDate === "")){
+                        eventDate += '-';
+                }
+                      eventDate += res[i];
+            }    
+          }        
+        } catch(err){
+          console.log('\x1b[31m%s\x1b[0m', 'Erreur regexp sur la date pour '+venue.name);
+        }
+        try{
+          const res = eve.match(regexName);
+          eventName = "";
+          for (let i = 1; i <= res.length-1; i++) {
+            if (!(res[i] === undefined)){
+              if (i > 1){
+                eventName += ' ';
+              }
+              eventName += res[i];      
+            }
+          }
+          eventName = removeBlanks(eventName);
+        }catch(err){
+          console.log('\x1b[31m%s\x1b[0m', 'Erreur regexp sur le nom de l\'événement pour '+venue.name);
+        }
+
+        const oldDate = eventDate;
+        console.log(eventDate);
+        eventDate = convertDate(eventDate,dateFormat);
+        if (!isValid(eventDate)){
+          console.log('\x1b[31m%s\x1b[0m', 'Format de date invalide pour '+venue.name+': '+oldDate);
+        }else{
+          unixDate = eventDate.getTime();
+                  //console.log(unixDate);
+        }
+
+
+        console.log(eventName);
+        if ('eventURL' in venue){
+          try{
+            eventURL = eve.match(regexURL)[1];
+            if (!(eventURL === undefined)){
+              eventURL = venue.baseURL+eventURL;
+            }
+            console.log(eventURL);
+          }catch(err){
+            console.log('\x1b[33m%s\x1b[0m', 'URL non fonctionnelle pour '+venue.name);
+          }
+          out = out+venue.name+','+eventName+','+unixDate+',100,'+eventURL+'\n';
+        }
+        console.log();
+      }  
+    }
+  }catch(error){
+    console.log("Pas de regex pour "+venue.name,error);
+  }
+  console.log("\n\n");
 }
+  
 
 
 
-function convertDate(s) {
+function convertDate(s,dateFormat) {
   s = s.normalize('NFD').replace(/[\u0300-\u036f]/g, ''); // remove accents
   for (const key in months) {
      for (const str of months[key]){
        s = s.replace(new RegExp(str,'i'),key);
     }
+  }  
+  if (s.includes('tonight')){
+    return new Date();
+  }else{
+    return parse(s, dateFormat, new Date());
   }
-  return s;
 }
 
 
 function removeBlanks(s){
   return s.replace(/[\n\t]/g, '').replace(/ {2,}/g, ' ').replace(/^ /,'');
 }
+
