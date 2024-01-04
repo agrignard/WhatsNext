160feaa11ea56cc94fe54605fded370a9e4d1339
Tri Nguyen-Huu <tri.nguyen-huu@ird.fr>
2024-01-04 13:54:10 +0100

    pré update majeur

--- scraping/analex.js
+++ scraping/analex.js
@@ -4,23 +4,39 @@ const cheerio = require('cheerio');
 
 // Chemin vers le fichier à lire
 //const fileName = 'Marché Gare.html';
-const fileName = 'test.html';
+//const fileName = 'terminal.html';
+const fileName = 'transbordeur.html';
 const sourcePath = './webSources/';
 
 // const eventNameStrings = ["dusty"];
 // const eventDateStrings = ["30","déc"];
 
-const eventNameStrings = ["giant"];
-const eventDateStrings = ["09","01"];
+// var eventNameStrings = ["Giant","atzur"];
+// var eventDateStrings = ["09.","01"];
+// var eventStyleStrings = ["pop-rock"];
+
+// var eventNameStrings = ["dusty"];
+// var eventDateStrings = ["30", "déc"];
+// var eventStyleStrings = [];
+
+var eventNameStrings = ["allien"];
+var eventDateStrings = ["06 janv."];
+var eventStyleStrings = ["techno"];
+
 
 const outputFile = "./outTest.html";
 
 
 
-fs.readFile(sourcePath+fileName, 'utf8')
+
+
+fileContent = fs.readFile(sourcePath+fileName, 'utf8')
 .then((fileContent) =>{
-    console.log('\x1b[32m%s\x1b[0m', `******* Analysing file: ${fileName}  *******`);
-    console.log(convertToLowerCase(fileContent));
+    console.log('\x1b[36m%s\x1b[0m', `******* Analysing file: ${fileName}  *******`);
+    // convert everything to lower case
+    eventNameStrings = eventNameStrings.map(string => string.toLowerCase());
+    eventDateStrings = eventDateStrings.map(string => string.toLowerCase());
+    eventStyleStrings = eventStyleStrings.map(string => string.toLowerCase());
     const $ = cheerio.load(convertToLowerCase(fileContent));
 
     let stringsToFind = eventNameStrings.concat(eventDateStrings);
@@ -29,89 +45,98 @@ fs.readFile(sourcePath+fileName, 'utf8')
     const tagsContainingStrings = $('*:contains("' + stringsToFind.join('"), :contains("') + '")')
         .filter((_, tag) => tagContainsAllStrings($(tag), stringsToFind));
 
+
+    
     //Affichez les noms des balises trouvées
     if (tagsContainingStrings.length === 0){
         console.log('\x1b[31m%s\x1b[0m',"No occurence found.");
     }else{
-        console.log("Found ",tagsContainingStrings.length," tags.");
-        tagsContainingStrings.each((_, tag) => {
-            //console.log('Balise trouvée:', tag.tagName,removeBlanks($(tag).text()+"\n\n"));
-            console.log('Tag found:', tag.tagName, $(tag).attr('class'),$(tag).text().length);
-        });
+        // tagsContainingStrings.each((_, tag) => {
+        //     console.log('Tag found:', tag.tagName, $(tag).attr('class'),$(tag).text().length);
+        // });
     
         // récupération de la meilleure balise
-        console.log("\nKeeping the best one:");
+       // console.log("\nKeeping the best one:");
         const tag = tagsContainingStrings.last();
-        console.log('\x1b[32m%s\x1b[0m', `Tag: <${tag.prop('tagName')} class="${$(tag).attr('class')}" id="${$(tag).attr('id')}">`);
-       
-        const groupContent = convertToLowerCase($(tag).text());
+        console.log("Found ",tagsContainingStrings.length,' tags. Best tag: \x1b[32m',
+             `<${tag.prop('tagName')} class="${$(tag).attr('class')}" id="${$(tag).attr('id')}">`,'\x1b[0m');
+        console.log('Contains the following information:\n');
+  //      console.log('\x1b[32m%s\x1b[0m', `Tag: <${tag.prop('tagName')} class="${$(tag).attr('class')}" id="${$(tag).attr('id')}">`);
+        const groupContent = $(tag).html();
+        console.log($(tag).text());
+        console.log();
         
-        console.log(removeBlanks(groupContent));
-//***************************** */
+     //   console.log(removeBlanks(groupContent));
+    //***************************************************************/
         // recherche des balises pour le nom de l'event
-        const $eventName = cheerio.load(groupContent);
-        console.log(eventNameStrings);
-        console.log('*:contains("' + eventNameStrings.join('"), :contains("') + '")');
-        const eventNameTagsContainingStrings = $eventName('*:contains("' + eventNameStrings.join('"), :contains("') + '")')
-        .filter((_, tag) => tagContainsAllStrings($(tag), eventNameStrings));
-        if (eventNameTagsContainingStrings.length === 0){
-            console.log('\x1b[31m%s\x1b[0m',"No tag matching the name of the event: ",eventNameStrings);
-        }else{
-            const eventNameTag = eventNameTagsContainingStrings.last();
-            console.log('\x1b[32m%s\x1b[0m', `Tag: <${eventNameTag.prop('tagName')} class="${$(eventNameTag).attr('class')}" id="${$(eventNameTag).attr('id')}">`);
-            console.log(removeBlanks($(eventNameTag).text()));
+        const $eventBlock = cheerio.load(groupContent);
+
+        const eventNameTags = eventNameStrings.map(string => findTag($eventBlock,string));
+        console.log("Event name tags:");
+        showTagsDetails(eventNameTags,$eventBlock);
+
+        const eventDateTags = eventDateStrings.map(string => findTag($eventBlock,string));
+        console.log("Event date tags:");
+        showTagsDetails(eventDateTags,$eventBlock);
+
+        if (eventStyleStrings.length > 0){
+            console.log("Event style tags:");
+            const eventStyleTags = eventStyleStrings.map(string => findTag($eventBlock,string));
+            showTagsDetails(eventStyleTags,$eventBlock);
         }
+
 
         console.log("\n\n\n");
         
       //  fs2.writeFileSync(outputFile, $(tag).html(), 'utf-8', { flag: 'w' });
     }
     
-    //console.log(removeBlanks($(tag).text()));
-    //console.log(tag);
-    // var mainTag = null;
-
-    // tagsContainingStrings.each((_, balise) => {
-    //     const texteBalise = $(balise).text();
-        
-    //     // Si la baliseAvecTexteLePlusCourt est null ou si le texte est plus court que celui actuellement retenu
-    //     if (!mainTag || texteBalise.length < $(mainTag).text().length) {
-    //         mainTag = balise;
-    //     }
-    //   });
-      
-    //   // Affichez le texte de la balise avec le texte le plus court
-    //   console.log('Texte de la balise avec le texte le plus court:', removeBlanks($(mainTag).text()));
-
-
-
-
-   // console.log(mainTag.text());
-   // console.log('Texte de la balise:', removeBlanks(mainTag.text()));
-//console.log('Code HTML de la balise:', mainTag.html());
-//console.log('Classe de la balise:', mainTag.attr('class'));
-//console.log('Identifiant de la balise:', mainTag.attr('id'));
-
-    // tagsContainingStrings.each((_, balise) => {
-    //     // Utilisation de .find() pour rechercher les descendants
-    //     const baliseInterne = $(balise).find(':contains("' + stringsToFind.join('"), :contains("') + '")').last();
-      
-    //     // Affichez le texte de la balise la plus interne
-    //     console.log('Texte de la balise la plus interne trouvée:', baliseInterne.text());
-    //   });
+   
+  
 
     
     console.log("\n\n\n\n\n");
 })
 .catch((erreur ) => {
-    console.error("Erreur d\'ouverture du fichier :",fileName, erreur);
+    console.error("Erreur de traitement du fichier :",fileName, erreur);
 });
 
+//*********************************************** */
+
+function showTagsDetails(tagList,source){
+    tagList.forEach(element => console.log('\x1b[32m', 'Tag: <', element.tagName,
+        '\x1b[32m class=\"',source(element).attr('class'),
+        '\x1b[32m\" id = \"',source(element).attr('id'),
+        '\x1b[32m\"> index: ',source(element).index(),'\x1b[0m ',removeBlanks(source(element).text())));
+}
 
 function tagContainsAllStrings(tag, strings) {
-    const tagContent = tag.text().toLowerCase(); // Convertir en minuscules
-    return strings.every(string => tagContent.includes(string.toLowerCase())); // Comparaison insensible à la casse
+    const tagContent = tag.text();//.toLowerCase(); // Convertir en minuscules
+    return strings.every(string => tagContent.includes(string)); // Comparaison insensible à la casse
 }
+
+function findTag(html,string) {
+    const tag = html(`*:contains('${string}')`).last();
+    return tag.length > 0 ? tag.get(0) : null;
+}
+
+
+function removeBlanks(s){
+    return s.replace(/ {2,}/g, ' ').replace(/\n[ \t\n]*/g, ' ');
+ //    return s.replace(/[\t]*/g, '').replace(/ {2,}/g, ' ').replace(/^ /,'').replace(/[\n]*/, '\n');
+ }
+ 
+ 
+ function convertToLowerCase(s){
+     regex = /^[^<]*?<|>([^]*?)<|>[^>]*?$/g;
+     return s.replace(regex,match => match.toLowerCase());
+ }
+ 
+ 
+
+
+
+
 
 
 
@@ -234,17 +259,4 @@ async function analyseFile(venue) {
 
 
 
-
-
-
-function removeBlanks(s){
-   return s.replace(/ {2,}/g, ' ').replace(/\n[ \t\n]*/g, ' ');
-//    return s.replace(/[\t]*/g, '').replace(/ {2,}/g, ' ').replace(/^ /,'').replace(/[\n]*/, '\n');
-}
-
-
-function convertToLowerCase(s){
-    regex = /^[^<]*?<|>([^]*?)<|>[^>]*?$/g;
-    return s.replace(regex,match => match.toLowerCase());
-}
 
